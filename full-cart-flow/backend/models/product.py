ac4f895c74dd81ffd2232734f572e6cda2df4c8e
tabish-605 <168262315+tabish-605@users.noreply.github.com>
2025-07-23 20:28:41 +0530

    Update product.py

--- full-cart-flow/backend/models/product.py
+++ full-cart-flow/backend/models/product.py
@@ -1,10 +1,10 @@
-from utils.db import db
+from utils.db import mongo
 import datetime
  
 class Product:
     @staticmethod
     def get_collection():
-        return db.products
+        return mongo.db.products
     
     @staticmethod
     def create_indexes():
@@ -13,25 +13,23 @@ class Product:
     
     @staticmethod
     def create_product(data):
-        # ... existing code ...
+        product = {
+            'name': data['name'],
+            'price': float(data['price']),
+            'description': data.get('description', ''),
+            'image': data.get('image', ''),
+            'category': data.get('category', 'Other'),
+            'rating': float(data.get('rating', 0)),
+            'stock': int(data.get('stock', 0)),
+            'created_at': datetime.datetime.utcnow(),
+            'updated_at': datetime.datetime.utcnow()
+        }
+        return Product.get_collection().insert_one(product)
     
     @staticmethod
     def find_all(filter_query=None):
-        # Return documents with ObjectId converted to string
-        products = list(Product.get_collection().find(filter_query))
-        for p in products:
-            p['_id'] = str(p['_id'])
-        return products
+        return list(Product.get_collection().find(filter_query, {'_id': 0}))
     
     @staticmethod
     def find_by_id(product_id):
-        from bson import ObjectId
-        try:
-            # Convert string ID to ObjectId
-            obj_id = ObjectId(product_id)
-            product = Product.get_collection().find_one({'_id': obj_id})
-            if product:
-                product['_id'] = str(product['_id'])
-            return product
-        except:
-            return None
+        return Product.get_collection().find_one({'id': product_id}, {'_id': 0})
