b49268e23c603323fb8ff322a947c1f2e99824e1
tabish-605 <168262315+tabish-605@users.noreply.github.com>
2025-07-23 20:06:35 +0530

    Update db.py

--- full-cart-flow/backend/utils/db.py
+++ full-cart-flow/backend/utils/db.py
@@ -1,12 +1,14 @@
-from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
  
 load_dotenv()
  
-mongo = PyMongo()
+client = None
+db = None
  
-def init_db(app):
-    app.config["MONGO_URI"] = os.getenv('MONGODB_URI')
-    mongo.init_app(app)
-    return mongo
+def init_db():
+    global client, db
+    client = MongoClient(os.getenv('MONGODB_URI'))
+    db = client.get_database()
+    return db
